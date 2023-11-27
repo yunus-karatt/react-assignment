89ca72415938fa50483212969232fed93f9fd3f8
yunus <yyas404@gmail.com>
2023-11-27 10:45:08 +0530

    Add form feature

--- form/src/App.js
+++ form/src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const inputRef = useRef("");
+
+  useEffect(()=>inputRef.current.focus())
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+    setName('')
+  };
+
   return (
     <div className="App">
-      
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>Enter you name:</label>
+        <input
+          ref={inputRef}
+          onChange={(e) => handleChange(e)}
+          type="text"
+          value={name}
+        />
+        <button>submit</button>
+      </form>
     </div>
   );
 }
